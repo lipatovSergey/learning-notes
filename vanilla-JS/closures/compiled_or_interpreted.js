855e681f494347d729a97986d403ebe88f27f358
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-04-16 19:17:29 +0300

    JS compiled or interpreted

--- vanilla-JS/closures/compiled_or_interpreted.js
+++ vanilla-JS/closures/compiled_or_interpreted.js
@@ -2,3 +2,17 @@ var greeting = "Hello"
 
 console.log(greeting)
 
+// greeting = ."Hi"
+
+// Программа не выведет "Hello", а выведет ошибку SyntaxError
+// это противоречит тому, как работают интерпретируемые языки
+
+
+// Ещё один пример 
+console.log("Howdy")
+saySomething("Hello", "Hi")
+
+function saySomething(greeting, greeting) {
+  "use strict"
+  console.log(greeting)
+}
