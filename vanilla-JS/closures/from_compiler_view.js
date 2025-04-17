8880e811a3d88d9d2f561a055ee44ea2985d3440
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-04-18 01:54:48 +0300

    continue to learn

---
+++ vanilla-JS/closures/from_compiler_view.js
@@ -0,0 +1,17 @@
+var students = [
+  { id: 14, name: 'Kyle' },
+  { id: 73, name: 'Suzy' },
+  { id: 112, name: 'Frank' },
+  { id: 6, name: 'Sarah' }
+]
+
+function getStudentName(studentID) {
+  for (let student of students) {
+    if (student.id === studentID) {
+      return student.name;
+    }
+  }
+}
+
+var nextStudent = getStudentName(73)
+console.log(nextStudent)
