df8c0856ddf098d15b6da1c96ca35fcf1eefb6ce
jovani <163670507+lipatovSergey@users.noreply.github.com>
2025-04-26 08:58:58 +0300

    var in functional scope

--- vanilla-JS/closures/from_compiler_view.js
+++ vanilla-JS/closures/from_compiler_view.js
@@ -1,9 +1,9 @@
 var students = [
-  { id: 14, name: 'Kyle' },
-  { id: 73, name: 'Suzy' },
-  { id: 112, name: 'Frank' },
-  { id: 6, name: 'Sarah' }
-]
+  { id: 14, name: "Kyle" },
+  { id: 73, name: "Suzy" },
+  { id: 112, name: "Frank" },
+  { id: 6, name: "Sarah" },
+];
 
 function getStudentName(studentID) {
   for (let student of students) {
@@ -13,5 +13,14 @@ function getStudentName(studentID) {
   }
 }
 
-var nextStudent = getStudentName(73)
-console.log(nextStudent)
+var nextStudent = getStudentName(73);
+console.log(nextStudent);
+
+function top() {
+  console.log(a);
+  function low() {
+    var a = "hi";
+  }
+}
+
+console.log(top());
